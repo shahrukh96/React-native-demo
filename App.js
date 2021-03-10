4c0bd07ab0f55620418b9caf2b250bad886107a3
shahrukhkarim <shahrukh96>
2021-03-10 23:57:26 +0530

    Completed the demo App

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, View, FlatList } from "react-native";
 
-export default function App() {
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+const App = () => {
+  const [goals, setGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalsHandler = (goal) => {
+    console.log(goal);
+    setGoals((currentGoal) => [
+      ...goals,
+      { id: Math.random().toString(), value: goal },
+    ]);
+    setIsAddMode(false);
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    console.log(goalId);
+    setGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button
+        title="Add New Goals"
+        color="#32a897"
+        onPress={() => setIsAddMode(true)}
+      />
+      <GoalInput
+        visible={isAddMode}
+        onCancel={cancelGoalAdditionHandler}
+        onAddGoal={addGoalsHandler}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={goals}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: { padding: 50 },
+  listItems: {
+    width: "70%",
+    padding: 10,
+    backgroundColor: "#6fa832",
+    marginVertical: 10,
+    borderRadius: 10,
   },
 });
+
+export default App;
